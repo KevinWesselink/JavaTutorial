1039b217b82f840e61fb3ec4bd4f482ae0f11cd0
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-06-27 09:57:03 +0200

    Mortgage berekenaar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterestRate = scanner.nextFloat();
+
+        System.out.print("Period (Years): ");
+        int period = scanner.nextInt();
+
+        double monthlyInterestRate = annualInterestRate / 100 / 12;
+        double mortgage = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, period))
+                / (Math.pow(1 + monthlyInterestRate, period) -1);
+
+        String mortgageFormat = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormat);
     }
 }
